a20acffbfdaeb303967f4e50f620bff6778c57c8
Himanshu Malhotra <malhotrahimanshu002@gmail.com>
2024-08-21 00:10:35 +0530

    Added Prefence Array For Subject

---
+++ Frontend Data/User_Search_Findables.js
@@ -0,0 +1,9 @@
+let findables_prefrences = {
+    HTML : ["Html" , "Html 5" , "Html And Css" , "HTML CSS AND JS" , "HTML , CSS And BootStrap" ] ,
+    CSS : ["Html" , "Html 5" , "Html And Css" , "HTML CSS AND JS" , "HTML , CSS And BootStrap" , "Figma" , "Web Desigining" , "Application Designing" , "Frontend Development" ] ,
+    JS : ["Html" , "Html 5" , "Html And Css" , "HTML CSS AND JS" , "HTML , CSS And BootStrap" ,"Angular Js" , "React Js" , "Node Js", ] ,
+    REACTJS : ["React" , "React Frontend Development" , "Frontend Development" , "React Redux"]
+}
+
+
+export default findables_prefrences;
--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+import findables_prefrences from "./Frontend Data/User_Search_Findables.js"
+
 const findTutorNavItem = document.getElementsByClassName("find-tutor-nav-item")
 const findTutorAnchor = document.getElementsByClassName("find-tutor-anchor")
 const finctutorItemsDiv = document.getElementsByClassName("find-tutor-extra-items")
+
 let showFindTutorItemsDiv = false
 console.log(findTutorAnchor)
 
@@ -37,3 +40,37 @@ findTutorNavItem[1].addEventListener("mouseout" , ()=>{
     }
     console.log(showFindTutorItemsDiv)
 })
+
+// Subject Input Show Prefrence Logic Here
+
+let SubjectInput = document.getElementById("subject_input")
+console.log(SubjectInput)
+
+function findPrefrenceArray(userInput){
+    let prefArray = []
+    for(let property in findables_prefrences ){
+        if(userInput == property){
+            prefArray = findables_prefrences[property]
+            break
+        }
+    }
+    return prefArray
+}
+
+SubjectInput.addEventListener("input",(e)=>{
+    let inputVal = e.target.value.toUpperCase()
+    let gotPrefArray = findPrefrenceArray(inputVal)
+    let prefDiv = document.getElementById("prefrence-items")
+    prefDiv.innerHTML = ""
+    if(gotPrefArray.length > 0){
+        for(let i = 0 ; i < gotPrefArray.length ; i++){
+            let anchorItem = document.createElement("a")
+            anchorItem.className = "p-1 hover:bg-blue-500"
+            anchorItem.innerText = gotPrefArray[i]
+            prefDiv.appendChild(anchorItem)
+        } 
+    }
+})
+
+
+
